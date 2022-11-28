1e65bbcac3c774977248f666f09b8ce0f99fc552
PreetishLabade <119239950+PreetishLabade@users.noreply.github.com>
2022-11-28 10:23:48 +0530

    Update and rename First_Program.py to 1_Install and import Modules.py

---
+++ 1_Install and import Modules.py
@@ -0,0 +1,12 @@
+#Below import command throws an error because pandas is not installed
+import pandas
+
+""""To install a module, go to CMD or powershell and write below command
+pip install pandas"""
+
+pip install pandas
+
+"""Now you can import the module in the code"""
+import pandas
+
+print("Hello World")
--- First_Program.py
+++
@@ -1,6 +0,0 @@
-#Below import commad throws an error because pandas is not installed
-""""To install a module, go to CMD or powershell and write below command
-pip install pandas"""
-import pandas
-
-print("Hello World")
