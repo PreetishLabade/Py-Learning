c85eeab5bd65b94a3e751cc69e3a87a575eb9880
PreetishLabade <119239950+PreetishLabade@users.noreply.github.com>
2022-11-28 12:31:59 +0530

    Add files via upload

---
+++ 3_Variable,  typecasting and input from user.py
@@ -0,0 +1,18 @@
+#Datatypes in Python
+a = 10
+print(type(a))
+
+b = 1.0145
+print(type(b))
+
+c = "Abc"
+print(type(c))
+
+d = True
+print(type(d))
+
+e = 9.86
+print("Enter a number")
+#Take input from user. By default, input is string. Hence we need to typecast it
+f = float(input())
+print("Output is ", e * f)
