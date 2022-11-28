30f971f283fea44c9f116c0abf7a4874b709962f
PreetishLabade <119239950+PreetishLabade@users.noreply.github.com>
2022-11-28 11:29:27 +0530

    Add files via upload

---
+++ tut6.py
@@ -0,0 +1,21 @@
+#Comment in python
+#To comment a single line , use #
+
+""""For multiline commenting,
+ Use 3 double inverted commas"""
+
+print("Hello World")
+"""By default, after every print statement, there is new line character"""
+
+#We can have multiple print statements by simply putting a comma
+
+a = "Boss"
+print("Hello World", "I am", a)
+
+print("Hi \n This is a new line")
+
+
+
+
+
+
